17cbb754cafb250740a47aa1cd92dd79dcfc1fc3
Kameron Ashley <kameronashley@gmail.com>
2021-09-23 12:14:05 -0700

    commit tree 111fc60fc90a617e86cc7cb366be040f91133922

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+function superbowlWin (array) {
+    let yearWon = array.find(resultIsWin);
+if (yearWon) {
+    return yearWon.year
+} else {
+    return yearWon};
+}
 
+function resultIsWin(teamObject) {
+    return teamObject.result === "W"
+}
